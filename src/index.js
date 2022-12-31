d5bdf01be91100d5bafa49dbf6635ae457d30ed5
Emrullah Celebi <cumali20182019@gmail.com>
2022-12-31 12:14:11 +0100

    v1

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { setItem } from "./utils/storage";
+import "./index.scss";
+import "bootstrap";
+
+document.getElementById("btnClick").addEventListener("click",()=>{
+   alert("Hello Webpack");
+
+setItem("token","sdoisjoisrtosigjsdffdfdfgertejkkukukihjgzxzxhkjghkuhk"); 
+});
+
+
+
+
+
+
